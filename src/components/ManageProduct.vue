d1448d17983ace5135f0dcc396151f806c880906
srabosthy <srabosthydas@gmail.com>
2021-04-18 21:48:43 +0600

    final commit

---
+++ src/components/ManageProduct.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+        <h1 class="display-4">Manage Product</h1>
+        <hr><hr>
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+
+</style>
